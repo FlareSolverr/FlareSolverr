a6467adb117fc87b62b39984e008037d8a547daf
xfouloux <sebastien.clemenceau@gmail.com>
2020-12-14 14:14:28 +1100

    Update anticaptcha.ts

--- src/captcha/anticaptcha.ts
+++ src/captcha/anticaptcha.ts
@@ -10,6 +10,8 @@ import { SolverOptions } from '.'
 
 export default async function solve({ url }: SolverOptions): Promise<string> {
   try {
+    const apikey = setAPIKey()
+    //switch between functions given captchatype
     const token = await solveCaptcha(url)
     return token
   } catch (e) {
