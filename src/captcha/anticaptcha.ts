405004fbaaf73e4dfc4b83bfc48f6bc685cc07c3
Sebastien Clemenceau <sebastien.clemenceau@offratel.net>
2020-12-16 14:07:58 +1100

    update for anti-captcha.com provider

--- src/captcha/anticaptcha.ts
+++ src/captcha/anticaptcha.ts
@@ -1,4 +1,7 @@
 const solveCaptcha = require('@antiadmin/anticaptchaofficial');
+const ANTI_CAPTCHA_APIKEY: string = process.env.ANTI_CAPTCHA_APIKEY || '0123456789abcdefghijklmnopqrstuvwxyz'
+let task = "";
+
 import { SolverOptions } from '.'
 /*
     This method uses the @antiadmin/anticaptchaofficial project:
@@ -8,43 +11,43 @@ import { SolverOptions } from '.'
         
 */
 
-export default async function solve({ url }: SolverOptions): Promise<string> {
+export default async function solve({ url }: SolverOptions, { sitekey }: SolverOptions, { type }: SolverOptions): Promise<string> {
   try {
     solveCaptcha.Setapikey(ANTI_CAPTCHA_APIKEY)
     solveCaptcha.getBalance()
-     .then(balance => console.log('my anti-captcha balance is $'+balance))
-     .catch(error => console.log('received error '+error))
+     .then((balance: string) => console.log('my anti-captcha balance is $'+balance))
+     .catch((error: string) => console.log('received error '+error))
     //switch between functions given captchatype
     switch(type) {
-      case funCaptcha:
-        const task = await solveCaptcha.solveFunCaptchaProxyless(url, sitekey)
-        .then(gresponse => {
+      case 'funCaptcha':
+        task = await solveCaptcha.solveFunCaptchaProxyless(url, sitekey)
+        .then((gresponse: string) => {
             console.log('result: '+gresponse);
         })
-        .catch(error => console.log('test received error '+error));
+        .catch((error: string) => console.log('test received error '+error));
         break;
-      case reCaptcha:
-        const task = await solveCaptcha.solveRecaptchaV3(url, 
+      case 'reCaptcha':
+        task = await solveCaptcha.solveRecaptchaV3(url, 
             sitekey,
             0.3, //minimum score required: 0.3, 0.7 or 0.9
             'PAGE_ACTION_CAN_BE_EMPTY')
-            .then(gresponse => {
+            .then((gresponse: string) => {
                 console.log('g-response: '+gresponse);
             })
-            .catch(error => console.log('test received error '+error));
+            .catch((error: string) => console.log('test received error '+error));
         break;
-      case hCaptcha:
-        const task = await solveCaptcha.solveHCaptchaProxyless(url, sitekey)
-            .then(gresponse => {
+      case 'hCaptcha':
+        task = await solveCaptcha.solveHCaptchaProxyless(url, sitekey)
+            .then((gresponse: string) => {
             console.log('g-response: '+gresponse);
             })
-            .catch(error => console.log('test received error '+error));
+            .catch((error: string) => console.log('test received error '+error));
         break;
       default:
         // code block
     }
     console.log('task result:' + task)
-    return task.solution
+    return task
   } catch (e) {
     console.error(e)
     return null
