fd8e967c8c34a92d2e17532194920e6941c86d8a
Jacob Price <63757465+jacobprice808@users.noreply.github.com>
2023-07-03 23:58:11 -0500

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import logging
 import os
 import re
 import shutil
+import urllib.parse
 
 from selenium.webdriver.chrome.webdriver import WebDriver
 import undetected_chromedriver as uc
@@ -14,13 +15,15 @@ USER_AGENT = None
 XVFB_DISPLAY = None
 PATCHED_DRIVER_PATH = None
 
+PROXY_EXTENSION_DIR = "proxy_extension"
+
 
 def get_config_log_html() -> bool:
-    return os.environ.get('LOG_HTML', 'false').lower() == 'true'
+    return os.environ.get("LOG_HTML", "false").lower() == "true"
 
 
 def get_config_headless() -> bool:
-    return os.environ.get('HEADLESS', 'true').lower() == 'true'
+    return os.environ.get("HEADLESS", "true").lower() == "true"
 
 
 def get_flaresolverr_version() -> str:
@@ -28,48 +31,130 @@ def get_flaresolverr_version() -> str:
     if FLARESOLVERR_VERSION is not None:
         return FLARESOLVERR_VERSION
 
-    package_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, 'package.json')
+    package_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), os.pardir, "package.json"
+    )
     if not os.path.isfile(package_path):
-        package_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'package.json')
+        package_path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "package.json"
+        )
     with open(package_path) as f:
-        FLARESOLVERR_VERSION = json.loads(f.read())['version']
+        FLARESOLVERR_VERSION = json.loads(f.read())["version"]
         return FLARESOLVERR_VERSION
 
 
-def get_webdriver(proxy: dict = None) -> WebDriver:
+def create_proxy_extension(proxy):
+    manifest_json = """
+    {
+        "version": "1.0.0",
+        "manifest_version": 2,
+        "name": "Chrome Proxy",
+        "permissions": [
+            "proxy",
+            "tabs",
+            "unlimitedStorage",
+            "storage",
+            "<all_urls>",
+            "webRequest",
+            "webRequestBlocking"
+        ],
+        "background": {"scripts": ["background.js"]},
+        "minimum_chrome_version": "76.0.0"
+    }
+    """
+
+    background_js = """
+    var config = {
+        mode: "fixed_servers",
+        rules: {
+            singleProxy: {
+                scheme: "http",
+                host: "%s",
+                port: %d
+            },
+            bypassList: ["localhost"]
+        }
+    };
+
+    chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+
+    function callbackFn(details) {
+        return {
+            authCredentials: {
+                username: "%s",
+                password: "%s"
+            }
+        };
+    }
+
+    chrome.webRequest.onAuthRequired.addListener(
+        callbackFn,
+        { urls: ["<all_urls>"] },
+        ['blocking']
+    );
+    """ % (
+        proxy["host"],
+        proxy["port"],
+        proxy["username"],
+        proxy["password"],
+    )
+
+    os.makedirs(PROXY_EXTENSION_DIR, exist_ok=True)
+
+    with open(os.path.join(PROXY_EXTENSION_DIR, "manifest.json"), "w") as f:
+        f.write(manifest_json)
+
+    with open(os.path.join(PROXY_EXTENSION_DIR, "background.js"), "w") as f:
+        f.write(background_js)
+
+
+def parse_proxy(proxy_str: str) -> dict:
+    parsed = urllib.parse.urlparse(proxy_str)
+    return {
+        "host": parsed.hostname,
+        "port": parsed.port,
+        "username": parsed.username,
+        "password": parsed.password,
+    }
+
+
+def get_webdriver(proxy_str: dict = None) -> WebDriver:
     global PATCHED_DRIVER_PATH
-    logging.debug('Launching web browser...')
+    logging.debug("Launching web browser...")
 
     # undetected_chromedriver
     options = uc.ChromeOptions()
-    options.add_argument('--no-sandbox')
-    options.add_argument('--window-size=1920,1080')
+    options.add_argument("--no-sandbox")
+    options.add_argument("--window-size=1920,1080")
     # todo: this param shows a warning in chrome head-full
-    options.add_argument('--disable-setuid-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("--disable-setuid-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     # this option removes the zygote sandbox (it seems that the resolution is a bit faster)
-    options.add_argument('--no-zygote')
+    options.add_argument("--no-zygote")
     # attempt to fix Docker ARM32 build
-    options.add_argument('--disable-gpu-sandbox')
-    options.add_argument('--disable-software-rasterizer')
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--ignore-ssl-errors')
+    options.add_argument("--disable-gpu-sandbox")
+    options.add_argument("--disable-software-rasterizer")
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--ignore-ssl-errors")
     # fix GL erros in ASUSTOR NAS
     # https://github.com/FlareSolverr/FlareSolverr/issues/782
     # https://github.com/microsoft/vscode/issues/127800#issuecomment-873342069
     # https://peter.sh/experiments/chromium-command-line-switches/#use-gl
-    options.add_argument('--use-gl=swiftshader')
+    options.add_argument("--use-gl=swiftshader")
 
-    if proxy and 'url' in proxy:
-        proxy_url = proxy['url']
-        logging.debug("Using webdriver proxy: %s", proxy_url)
-        options.add_argument('--proxy-server=%s' % proxy_url)
+    if proxy_str:
+        proxy = parse_proxy(proxy_str)
+        if all(key in proxy for key in ["host", "port", "username", "password"]):
+            create_proxy_extension(proxy)
+            options.add_argument(
+                "--load-extension=%s" % os.path.abspath(PROXY_EXTENSION_DIR)
+            )
 
     # note: headless mode is detected (options.headless = True)
     # we launch the browser in head-full mode with the window hidden
     windows_headless = False
     if get_config_headless():
-        if os.name == 'nt':
+        if os.name == "nt":
             windows_headless = True
         else:
             start_xvfb_display()
@@ -90,14 +175,24 @@ def get_webdriver(proxy: dict = None) -> WebDriver:
 
     # downloads and patches the chromedriver
     # if we don't set driver_executable_path it downloads, patches, and deletes the driver each time
-    driver = uc.Chrome(options=options, browser_executable_path=browser_executable_path,
-                       driver_executable_path=driver_exe_path, version_main=version_main,
-                       windows_headless=windows_headless)
+    driver = uc.Chrome(
+        options=options,
+        browser_executable_path=browser_executable_path,
+        driver_executable_path=driver_exe_path,
+        version_main=version_main,
+        windows_headless=windows_headless,
+    )
 
     # save the patched driver to avoid re-downloads
     if driver_exe_path is None:
-        PATCHED_DRIVER_PATH = os.path.join(driver.patcher.data_path, driver.patcher.exe_name)
+        PATCHED_DRIVER_PATH = os.path.join(
+            driver.patcher.data_path, driver.patcher.exe_name
+        )
         shutil.copy(driver.patcher.executable_path, PATCHED_DRIVER_PATH)
+
+    # Clean up proxy extension directory
+    if proxy_str:
+        shutil.rmtree(PROXY_EXTENSION_DIR)
 
     # selenium vanilla
     # options = webdriver.ChromeOptions()
@@ -115,15 +210,21 @@ def get_chrome_exe_path() -> str:
     if CHROME_EXE_PATH is not None:
         return CHROME_EXE_PATH
     # linux pyinstaller bundle
-    chrome_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'chrome', "chrome")
+    chrome_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "chrome", "chrome"
+    )
     if os.path.exists(chrome_path):
         if not os.access(chrome_path, os.X_OK):
-            raise Exception(f'Chrome binary "{chrome_path}" is not executable. '
-                            f'Please, extract the archive with "tar xzf <file.tar.gz>".')
+            raise Exception(
+                f'Chrome binary "{chrome_path}" is not executable. '
+                f'Please, extract the archive with "tar xzf <file.tar.gz>".'
+            )
         CHROME_EXE_PATH = chrome_path
         return CHROME_EXE_PATH
     # windows pyinstaller bundle
-    chrome_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'chrome', "chrome.exe")
+    chrome_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "chrome", "chrome.exe"
+    )
     if os.path.exists(chrome_path):
         CHROME_EXE_PATH = chrome_path
         return CHROME_EXE_PATH
@@ -137,7 +238,7 @@ def get_chrome_major_version() -> str:
     if CHROME_MAJOR_VERSION is not None:
         return CHROME_MAJOR_VERSION
 
-    if os.name == 'nt':
+    if os.name == "nt":
         # Example: '104.0.5112.79'
         try:
             complete_version = extract_version_nt_executable(get_chrome_exe_path())
@@ -155,26 +256,28 @@ def get_chrome_major_version() -> str:
         complete_version = process.read()
         process.close()
 
-    CHROME_MAJOR_VERSION = complete_version.split('.')[0].split(' ')[-1]
+    CHROME_MAJOR_VERSION = complete_version.split(".")[0].split(" ")[-1]
     return CHROME_MAJOR_VERSION
 
 
 def extract_version_nt_executable(exe_path: str) -> str:
     import pefile
+
     pe = pefile.PE(exe_path, fast_load=True)
     pe.parse_data_directories(
         directories=[pefile.DIRECTORY_ENTRY["IMAGE_DIRECTORY_ENTRY_RESOURCE"]]
     )
-    return pe.FileInfo[0][0].StringTable[0].entries[b"FileVersion"].decode('utf-8')
+    return pe.FileInfo[0][0].StringTable[0].entries[b"FileVersion"].decode("utf-8")
 
 
 def extract_version_nt_registry() -> str:
     stream = os.popen(
-        'reg query "HKLM\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Google Chrome"')
+        'reg query "HKLM\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Google Chrome"'
+    )
     output = stream.read()
-    google_version = ''
-    for letter in output[output.rindex('DisplayVersion    REG_SZ') + 24:]:
-        if letter != '\n':
+    google_version = ""
+    for letter in output[output.rindex("DisplayVersion    REG_SZ") + 24 :]:
+        if letter != "\n":
             google_version += letter
         else:
             break
@@ -184,17 +287,21 @@ def extract_version_nt_registry() -> str:
 def extract_version_nt_folder() -> str:
     # Check if the Chrome folder exists in the x32 or x64 Program Files folders.
     for i in range(2):
-        path = 'C:\\Program Files' + (' (x86)' if i else '') + '\\Google\\Chrome\\Application'
+        path = (
+            "C:\\Program Files"
+            + (" (x86)" if i else "")
+            + "\\Google\\Chrome\\Application"
+        )
         if os.path.isdir(path):
             paths = [f.path for f in os.scandir(path) if f.is_dir()]
             for path in paths:
                 filename = os.path.basename(path)
-                pattern = '\d+\.\d+\.\d+\.\d+'
+                pattern = "\d+\.\d+\.\d+\.\d+"
                 match = re.search(pattern, filename)
                 if match and match.group():
                     # Found a Chrome version.
                     return match.group(0)
-    return ''
+    return ""
 
 
 def get_user_agent(driver=None) -> str:
@@ -218,6 +325,7 @@ def start_xvfb_display():
     global XVFB_DISPLAY
     if XVFB_DISPLAY is None:
         from xvfbwrapper import Xvfb
+
         XVFB_DISPLAY = Xvfb()
         XVFB_DISPLAY.start()
 
@@ -225,4 +333,4 @@ def start_xvfb_display():
 def object_to_dict(_object):
     json_dict = json.loads(json.dumps(_object, default=lambda o: o.__dict__))
     # remove hidden fields
-    return {k: v for k, v in json_dict.items() if not k.startswith('__')}
+    return {k: v for k, v in json_dict.items() if not k.startswith("__")}
