bc615fbe8c19208838b6612cef391e9731a168e0
Andrew Corbin <andrew.c.corbin@gmail.com>
2023-08-01 12:52:59 +0000

    Commented out options that I didn't need. One or more of them also cause the workaround to fail, have to narrow down which one(s) still.

--- src/utils.py
+++ src/utils.py
@@ -42,41 +42,47 @@ def get_webdriver(proxy: dict = None) -> WebDriver:
 
     # undetected_chromedriver
     options = uc.ChromeOptions()
-    options.add_argument('--no-sandbox')
-    options.add_argument('--window-size=1920,1080')
+#    options.add_argument('--no-sandbox')
+#    options.add_argument('--window-size=1920,1080')
     # todo: this param shows a warning in chrome head-full
-    options.add_argument('--disable-setuid-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
+#   options.add_argument('--disable-setuid-sandbox')
+#   options.add_argument('--disable-dev-shm-usage')
     # this option removes the zygote sandbox (it seems that the resolution is a bit faster)
-    options.add_argument('--no-zygote')
+ #  options.add_argument('--no-zygote')
     # attempt to fix Docker ARM32 build
-    options.add_argument('--disable-gpu-sandbox')
-    options.add_argument('--disable-software-rasterizer')
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--ignore-ssl-errors')
+#   options.add_argument('--disable-gpu-sandbox')
+#   options.add_argument('--disable-software-rasterizer')
+    #options.add_argument('--ignore-certificate-errors')
+    #options.add_argument('--ignore-ssl-errors')
     # fix GL errors in ASUSTOR NAS
     # https://github.com/FlareSolverr/FlareSolverr/issues/782
     # https://github.com/microsoft/vscode/issues/127800#issuecomment-873342069
     # https://peter.sh/experiments/chromium-command-line-switches/#use-gl
-    options.add_argument('--use-gl=swiftshader')
+    #options.add_argument('--use-gl=swiftshader')
     # workaround for updated 'verify your are human' check
     # https://github.com/FlareSolverr/FlareSolverr/issues/811
     options.add_argument('--auto-open-devtools-for-tabs')
-    options.add_argument('--headless=true')
+#    options.add_argument('--headless=true')
+    options.add_argument("--disable-popup-blocking")
+
+    options.headless = True
+    options.add_argument( '--headless' )
+    
+
 
     if proxy and 'url' in proxy:
         proxy_url = proxy['url']
         logging.debug("Using webdriver proxy: %s", proxy_url)
-        options.add_argument('--proxy-server=%s' % proxy_url)
+        #options.add_argument('--proxy-server=%s' % proxy_url)
 
     # note: headless mode is detected (options.headless = True)
     # we launch the browser in head-full mode with the window hidden
-    windows_headless = False
-    if get_config_headless():
-        if os.name == 'nt':
-            windows_headless = True
-        else:
-            start_xvfb_display()
+    #windows_headless = False
+    #if get_config_headless():
+    #    if os.name == 'nt':
+    #        windows_headless = True
+    #    else:
+    #        start_xvfb_display()
 
     # if we are inside the Docker container, we avoid downloading the driver
     driver_exe_path = None
@@ -90,25 +96,26 @@ def get_webdriver(proxy: dict = None) -> WebDriver:
             driver_exe_path = PATCHED_DRIVER_PATH
 
     # detect chrome path
-    browser_executable_path = get_chrome_exe_path()
+    browser_executable_path = str('/bin/chromium')
+#    browser_executable_path = get_chrome_exe_path()
 
     # downloads and patches the chromedriver
     # if we don't set driver_executable_path it downloads, patches, and deletes the driver each time
-    driver = uc.Chrome(options=options, browser_executable_path=browser_executable_path,
-                       driver_executable_path=driver_exe_path, version_main=version_main,
-                       windows_headless=windows_headless)
-
+ #   driver = uc.Chrome(options=options, browser_executable_path=browser_executable_path,
+  #                     driver_executable_path=driver_exe_path, version_main=version_main,
+  #                     windows_headless=windows_headless)
+    driver = uc.Chrome( options = options)
     # save the patched driver to avoid re-downloads
-    if driver_exe_path is None:
-        PATCHED_DRIVER_PATH = os.path.join(driver.patcher.data_path, driver.patcher.exe_name)
-        shutil.copy(driver.patcher.executable_path, PATCHED_DRIVER_PATH)
+#    if driver_exe_path is None:
+#        PATCHED_DRIVER_PATH = os.path.join(driver.patcher.data_path, driver.patcher.exe_name)
+#        shutil.copy(driver.patcher.executable_path, PATCHED_DRIVER_PATH)
         
     # workaround for updated 'verify your are human' check
     # https://github.com/FlareSolverr/FlareSolverr/issues/811
-    driver.execute_script('''window.open("","_blank");''')
-    driver.switch_to.window(window_name=driver.window_handles[0])
-    driver.close()
-    driver.switch_to.window(window_name=driver.window_handles[0])
+#    driver.execute_script('''window.open("","_blank");''')
+#    driver.switch_to.window(window_name=driver.window_handles[0])
+#    driver.close()
+#    driver.switch_to.window(window_name=driver.window_handles[0])
 
     # selenium vanilla
     # options = webdriver.ChromeOptions()
