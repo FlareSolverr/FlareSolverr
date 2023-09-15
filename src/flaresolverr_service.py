4139e8d47c1365d7a440e17dda1786c1d0517319
ilike2burnthing <59480337+ilike2burnthing@users.noreply.github.com>
2023-09-15 20:37:26 +0100

    Update checkbox selector, again

--- src/flaresolverr_service.py
+++ src/flaresolverr_service.py
@@ -256,7 +256,7 @@ def click_verify(driver: WebDriver):
         driver.switch_to.frame(iframe)
         checkbox = driver.find_element(
             by=By.XPATH,
-            value='//*[@id="challenge-stage"]/div/label/map/img',
+            value='//*[@id="challenge-stage"]/div/label/input',
         )
         if checkbox:
             actions = ActionChains(driver)
