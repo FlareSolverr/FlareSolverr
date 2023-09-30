1cefd9eaea54fb788318867a734aa483ce4738ed
Thiago Oliveira <thiceconelo@gmail.com>
2023-09-30 15:52:41 -0300

    Fix of the subprocess.STARTUPINFO() call.

--- src/undetected_chromedriver/__init__.py
+++ src/undetected_chromedriver/__init__.py
@@ -451,8 +451,9 @@ class Chrome(selenium.webdriver.chrome.webdriver.WebDriver):
                 options.binary_location, *options.arguments
             )
         else:
-            startupinfo = subprocess.STARTUPINFO()
+            startupinfo = None
             if os.name == 'nt' and windows_headless:
+                startupinfo = subprocess.STARTUPINFO()
                 startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
             browser = subprocess.Popen(
                 [options.binary_location, *options.arguments],
