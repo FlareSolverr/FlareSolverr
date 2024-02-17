f9de88b718e67da53af2a73d5cc3af5a9058dd08
ilike2burnthing <59480337+ilike2burnthing@users.noreply.github.com>
2024-02-17 00:44:03 +0000

    Update build_package.py

--- src/build_package.py
+++ src/build_package.py
@@ -25,7 +25,7 @@ def clean_files():
 
 def download_chromium():
     # https://commondatastorage.googleapis.com/chromium-browser-snapshots/index.html?prefix=Linux_x64/
-    revision = "1140001" if os.name == 'nt' else '1140000'
+    revision = "1250005" if os.name == 'nt' else '1250006'
     arch = 'Win_x64' if os.name == 'nt' else 'Linux_x64'
     dl_file = 'chrome-win' if os.name == 'nt' else 'chrome-linux'
     dl_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, 'dist_chrome')
