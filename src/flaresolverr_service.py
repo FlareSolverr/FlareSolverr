f7f65cf7868035550cda1f05a73691d239735d4f
AlexFozor <alex.fozor@gmail.com>
2024-08-01 15:41:50 +0200

    headers fix

--- src/flaresolverr_service.py
+++ src/flaresolverr_service.py
@@ -368,7 +368,7 @@ def _evil_logic(req: V1RequestBase, driver: ChromiumPage, method: str) -> Challe
         challenge_res.status = data.response.status
         if not req.returnOnlyCookies:
             challenge_res.response = data.response.body
-            challenge_res.headers = data.response.headers.copy()
+            challenge_res.headers = dict(data.response.headers)
 
     challenge_res.cookies = driver.cookies()
     challenge_res.userAgent = req.userAgent or utils.get_user_agent(driver)
