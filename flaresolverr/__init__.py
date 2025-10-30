e34f73eed9a1267f0fc603f1d1371cba6001cb10
Péter Tombor <public@tmbpeter.com>
2025-10-30 10:37:12 +0100

    Expose resolver functions to python package
    Move web server startup to separate function
    
    This makes it possible to call FlareSolverr functions directly from python code without having to start up a webserver.

--- flaresolverr/__init__.py
+++ flaresolverr/__init__.py
@@ -1,1 +1,17 @@
+"""FlareSolverr is a proxy server to bypass Cloudflare and DDoS-GUARD protection."""
+
+from .flaresolverr import init, start_webserver
+from .utils import get_chrome_exe_path
+from .dtos import V1RequestBase, ChallengeResolutionT
+from .flaresolverr_service import resolve_challenge
+
 __version__ = "3.4.3"
+
+__all__ = [
+    "init",
+    "start_webserver",
+    "get_chrome_exe_path",
+    "V1RequestBase",
+    "ChallengeResolutionT",
+    "resolve_challenge",
+]
